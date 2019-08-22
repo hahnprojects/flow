adc868e963fa53545a965dcc3743f81e8407592d
Sebastian Copei <seb@uni-kassel.de>
2019-08-22 12:00:24 +0200

    fix: add raw output for content data

--- src/content.service.ts
+++ src/content.service.ts
@@ -7,7 +7,11 @@ export class ContentService extends DataService<Content> {
     super(httpClient, 'api/contents');
   }
 
-  download = (id: string): Promise<Blob> => {
-    return this.httpClient.get<Blob>(`${this.basePath}/${id}/download`, { responseType: 'blob' });
+  download = (id: string, raw: boolean = false): Promise<Blob | ArrayBuffer> => {
+    if (raw) {
+      return this.httpClient.get<Blob>(`${this.basePath}/${id}/download`, { responseType: 'arraybuffer' });
+    } else {
+      return this.httpClient.get<Blob>(`${this.basePath}/${id}/download`, { responseType: 'blob' });
+    }
   };
 }
