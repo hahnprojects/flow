f75a8c0f011cd4ef274c810037bc06b5f5f022f3
Jan <jst@linux.com>
2020-01-28 15:36:09 +0100

    test: add simple test

---
+++ modules/example/src/simple.spec.ts
@@ -0,0 +1,17 @@
+import { FlowEvent } from '@hahnpro/flow-sdk';
+
+import { DoNothing } from './DoNothing';
+
+/* tslint:disable:no-console */
+describe('Simple', () => {
+  test('simple test', async (done) => {
+    const nothingHappens = new DoNothing({ id: 'nothing' }, { logData: false });
+
+    const event = await nothingHappens.noop(new FlowEvent({ id: 'trigger' }, { foo: 'bar' }));
+    const data = event.getData();
+    expect(data).toBeDefined();
+    expect(data.foo).toEqual('bar');
+
+    done();
+  });
+});
