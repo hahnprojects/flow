9ce8dbb671d53823d46c22e59c642b477175e1d5
Jan <jst@linux.com>
2020-02-11 00:28:05 +0100

    feat: add more examples

--- modules/example/index.ts
+++ modules/example/index.ts
@@ -1,9 +1,11 @@
 import { FlowModule } from '@hahnpro/flow-sdk';
 
 import { DoNothing } from './src/DoNothing';
+import { DoSomething } from './src/DoSomething';
+import { ModifySomething } from './src/ModifySomething';
 
 @FlowModule({
   name: 'example',
-  declarations: [DoNothing],
+  declarations: [DoNothing, DoSomething, ModifySomething],
 })
 export default class ExampleModule {}
