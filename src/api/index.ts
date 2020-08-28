e5073c31cc76ac4b42b5dc7de2437b8bf44577ea
Jan <jst@linux.com>
2020-08-28 16:13:38 +0200

    fix: export data interface

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 export * from './api';
 export * from './asset.interface';
 export * from './content.interface';
+export * from './data.interface';
 export * from './data.service';
 export * from './secret.interface';
 export * from './timeseries.interface';
