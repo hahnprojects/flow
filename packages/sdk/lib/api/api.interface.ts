d81673c98e82a595a658539604cdfc9cf9c5100c
Jan <jst@linux.com>
2020-09-10 21:45:08 +0200

    chore: release
     - @hahnpro/flow-cli@2.2.0
     - @hahnpro/flow-sdk@3.7.0
    
    .

--- packages/sdk/lib/api/api.interface.ts
+++ packages/sdk/lib/api/api.interface.ts
@@ -1,13 +1,12 @@
 import { AssetInterface } from './asset.interface';
 import { ContentInterface } from './content.interface';
 import { SecretInterface } from './secret.interface';
-import { SidriveiqInterface } from './sidriveiq.interface';
 import { TimeseriesInterface } from './timeseries.interface';
 
 export interface APIInterface {
   assetManager: AssetInterface;
   contentManager: ContentInterface;
   secretsManager: SecretInterface;
-  sidriveManager: SidriveiqInterface;
+  sidriveManager: any;
   timeSeriesManager: TimeseriesInterface;
 }
