70df775c75467329fc47f2afa967684d15085690
Xavier Ngansop <ngaxavi@gmail.com>
2021-08-03 13:15:39 +0200

    fix: update timeseries and asset interface

--- packages/sdk/lib/api/asset.interface.ts
+++ packages/sdk/lib/api/asset.interface.ts
@@ -5,12 +5,17 @@ export interface Asset {
   id?: string;
   name: string;
   type: string | AssetType;
+  type$name?: string;
   readPermissions: string[];
   readWritePermissions: string[];
   tags?: string[];
   parent?: any | Asset;
+  parent$name?: string;
   data?: any;
   attachments?: string[];
+  image?: string;
+  actions?: string[];
+  notificationEndpoints?: string[];
   createdAt?: string;
   updatedAt?: string;
 }
@@ -19,10 +24,16 @@ export interface AssetType {
   id?: string;
   name: string;
   allowedParent?: string;
+  allowedParent$name?: string;
   readPermissions: string[];
   readWritePermissions: string[];
   typeSchema: any;
+  supertype?: string;
+  supertype$name?: string;
   uiSchema: any;
+  actions?: string[];
+  createdAt?: string;
+  updatedAt?: string;
 }
 
 export interface Attachment {
@@ -43,6 +54,9 @@ export interface Action {
   isAuthFromAsset?: boolean;
   url?: string;
   data?: string;
+  responseType?: string;
+  readPermissions: string[];
+  readWritePermissions: string[];
 }
 
 export interface AssetInterface extends DataInterface<Asset> {
