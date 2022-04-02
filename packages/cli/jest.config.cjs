ab5a691e2c73560d58d760e842f68da3d7097093
Jan <jst@linux.com>
2022-04-02 18:49:56 +0200

    feat(cli): support auth for regular user accounts
    instead of just service accounts

--- packages/cli/jest.config.cjs
+++ packages/cli/jest.config.cjs
@@ -3,4 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/*.spec.mjs'],
   transform: {},
+  moduleNameMapper: {
+    chalk: 'chalk/source/index.js',
+    '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
+    '#supports-color': 'chalk/source/vendor/supports-color/index.js',
+  },
 };
